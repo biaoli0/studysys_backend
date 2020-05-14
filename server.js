fc7779bfd5b31e03c5da456ba2bc41781dfd0705
MARS_Penguine <lbit123@outlook.com>
2020-05-14 11:44:27 +1000

    Add CORS and JWT

--- server.js
+++ server.js
@@ -3,12 +3,16 @@ const app = new Koa();
 const Router = require('koa-router');
 const router = new Router();
 const jwt = require('jsonwebtoken');
-
 const session = require('koa-session');
 
 const bodyParser = require('koa-bodyparser');
 app.use(bodyParser());
 
+// CORS
+const cors = require('@koa/cors');
+app.use(cors());
+
+// Router allowedMethods
 app.use(router.routes()).use(router.allowedMethods());
 
 // Session config
@@ -31,31 +35,54 @@ const user = {
     password: '123123',
 }
 
-// Vertify user and generate a token in session
+const privateKey = 'my_token';
+
+// Check Token
 app.use(async (ctx, next) => {
-    userId = ctx.headers.userId;
-    password = ctx.headers.password;
-
-    if (userId === user.id && password === user.password) {
-        const token = jwt.sign({
-            name: user.name,
-            id: user.id,
-        }, 'my_token', {
-            expiresIn: '2h'
-        });
-        ctx.session.token = token;
-        ctx.status = 200;
-    } else {
-        ctx.status = 555;
-    }
-
+    const token = ctx.headers.token;
+    jwt.verify(token, privateKey, (err, decoded) => {
+        // Token expired or undefined
+        if (err) {
+            // No user id and password
+            if (ctx.headers.user_id === undefined && ctx.headers.password === undefined) {
+                ctx.status = 555;
+                ctx.session.message = "Token expired";
+                return;
+            }
+        }
+    })
     await next();
 })
 
-// GET request
-router.get('/', async (ctx, next) => {
+// Vertify user and generate a token in session
+app.use(async (ctx, next) => {
+    if (ctx.headers.token === undefined) {
+        const userId = ctx.headers.user_id;
+        const password = ctx.headers.password;
+        if (userId === user.id && password === user.password) {
+            const token = jwt.sign({
+                name: user.name,
+                id: userId,
+            }, privateKey, {
+                expiresIn: '1h',
+            });
+            ctx.session.token = token;
+            ctx.status = 200;
+            ctx.session.message = "登录成功";
+        } else {
+            ctx.status = 555;
+            ctx.session.message = "用户密码错误";
+            return;
+        }
+    }
+    await next();
+})
+
+// POST request
+router.post('/', async (ctx, next) => {
     ctx.body = {
         token: ctx.session.token,
+        message: ctx.session.message,
     };
     await next();
 })
